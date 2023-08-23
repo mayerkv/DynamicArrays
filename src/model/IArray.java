9639c67c70b2aca32bbb5da30d1074d6ba1068b6
kmayer <k.v.mayer@tm2.etagi.com>
2023-08-23 08:31:58 +0500

    initial commit

---
+++ src/model/IArray.java
@@ -0,0 +1,11 @@
+package model;
+
+public interface IArray<T> {
+    int size();
+    void add(T item);
+    T get(int index);
+
+//     void add(T item, int index); // with shift to tail
+//
+//    T remove(int index); // return deleted element
+}
